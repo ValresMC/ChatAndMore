8584a25bbdfa69155ab263f4da89ba9dd0c410ef
Valres <104681026+ValresMC@users.noreply.github.com>
2023-12-04 05:19:47 +0100

    Update Utils.php

--- src/ChatPlus/Utils.php
+++ src/ChatPlus/Utils.php
@@ -7,8 +7,13 @@ class Utils
     public static bool $lock = false;
 
     public static array $cooldown = [];
+    public static array $badword = [];
+    public static array $messages = [];
 
-    public function __construct(public Main $plugin) {}
+    public function __construct(public Main $plugin)
+    {
+        $this->loadConfig();
+    }
 
     public static function isLock(): bool
     {
@@ -18,5 +23,22 @@ class Utils
     public static function setLock(bool $lock): void
     {
         self::$lock = $lock;
+    }
+
+    public function loadConfig(): void
+    {
+        $config = $this->plugin->config();
+
+        foreach($config->get("bad-words") as $word){
+            if(!isset(self::$badword[$word])){
+                self::$badword[] = $word;
+            }
+        }
+
+        foreach($config->get("messages") as $message){
+            if(!isset(self::$messages[$message])){
+                self::$messages[] = $message;
+            }
+        }
     }
 }
